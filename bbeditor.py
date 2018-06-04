d0c54bc9c8396c8f596ddfd5b2da510b56d7f3ec
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-06-04 15:51:39 +0900

    editorの仕様ほぼ完了(テスト前)

--- bbeditor.py
+++ bbeditor.py
@@ -15,6 +15,17 @@ keyList = [K_a, K_b, K_c, K_d, K_e, K_f, K_g, K_h, K_i, K_j, K_k, K_l, K_m,
 
 CAN_BREAK, CANNOT_BREAK = [0, 1]
 
+def toletter(num):
+    """数字→文字"""
+    if num < 26: return chr(num + 65)
+    else: return chr(num - 26 + 97)
+
+def tonum(letter):
+    """文字→数字(kind)"""
+    x = ord(letter)
+    if x < 97: return x - 65
+    else: return x - 97 + 26
+
 class editor():
     def __init__(self):
         pygame.init()
@@ -34,19 +45,42 @@ class editor():
             line = []
             for j in range(22): line.append(0)
             self.occupy.append(line)
+        self.saved = True # Falseだと移るときに警告
+        self.current_stage = 1  # デフォルトではSTAGE1が選択されている
 
         self.loading()
+        self.load_stage(1)  # とりあえずSTAGE1のデータを放り込む。
+
         clock = pygame.time.Clock()
 
         while True:
-            screen.blit(self.backImg, (0, 0))
-            self.blocks.draw(screen)
+            screen.fill((0, 0, 0))
+            clock.tick(60)
+            self.draw(screen)
             pygame.display.update()
             self.key_handler()
 
     def loading(self):
         """背景、ブロック画像のロード"""
+        # 背景
         self.backImg = self.load_image("editor_bg")
+
+        # ステージボタン
+        self.stage_button = []
+        imageList = self.load_image("SELECTSTAGE")
+        for i in range(25):
+            surface = pygame.Surface((20, 20))
+            surface.blit(imageList, (0, 0), (20 * i, 0, 20, 20))
+            self.stage_button.append(surface)
+
+        # セーブボタン
+        self.save_button = []
+        imageList = self.load_image("SAVE")
+        for i in range(2):
+            surface = pygame.Surface((80, 40))
+            surface.blit(imageList, (0, 0), (0, 40 * i, 80, 40))
+            self.save_button.append(surface)
+
         # 壊せる0～4の2×1型。
         imageList = self.load_image("blockimages1")
         for i in range(5):
@@ -103,6 +137,24 @@ class editor():
             surface.blit(imageList, (0, 0), (20 * i, 0, 20, 20 * (i + 1)))
             block.images.append(surface)
 
+    def draw(self, screen):
+        screen.blit(self.backImg, (0, 0))
+        # ステージ表示用ボタン
+        for k in range(2):
+            for i in range(15):
+                if k == 1 and i >= 10: break
+                screen.blit(self.stage_button[k * 15 + i], (10 + i * 20, 270 + k * 20))
+
+        # 選択中のステージ
+        screen.blit(self.stage_button[self.current_stage - 1], (320, 270))
+
+        # SAVEボタン（savedなら青、not savedなら赤。）
+        flag = 0
+        if not self.saved: flag = 1
+        screen.blit(self.save_button[flag], (350, 270))
+        # ブロック
+        self.blocks.draw(screen)
+
     def key_handler(self):
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -110,6 +162,25 @@ class editor():
                 sys.exit()
             elif event.type == KEYDOWN:
                 self.keydown_event(event.key)
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                # ステージ選択及びセーブをマウスクリックで行う
+                x, y = event.pos
+                # 0とか1～25を返す、-1のときは何も起きない。
+                event_code = self.calc_clickevent(x, y)
+                if event_code < 0: return
+                elif event_code == 0:
+                    # 0のときはセーブする
+                    if self.saved: return
+                    self.save_stage(); self.saved = True
+                else:
+                    if event_code == self.current_stage: return
+                    else:
+                        # セーブしてない時はセーブしてから移る
+                        if not self.saved:
+                            self.save_stage(); self.saved = True
+                        # 1～25のときはステージを更新
+                        self.load_stage(event_code)
+                        self.current_stage = event_code  # 選択ステージを更新
 
     def keydown_event(self, key):
         """キーイベント"""
@@ -120,27 +191,38 @@ class editor():
             # ブロックの種類の切り替え
             if self.mode == CANNOT_BREAK: self.mode = CAN_BREAK
             else: self.mode = CANNOT_BREAK
-        elif key == K_RETURN:
-            # マップの保存
-            self.save()
-            self.map_init()   # 初期化
-            self.blocks.empty()  # 初期化
+        elif key == K_BACKSPACE:
+            # 最後に保存した時の状態に戻す
+            if self.saved: return
+            self.load_stage(self.current_stage)
+            self.saved = True
         else:
             x, y = pygame.mouse.get_pos()
             x = x // GS; y = y // GS
             if key == K_DELETE:
-                self.delete_block((x, y))
+                # ブロックの削除
+                if self.delete_block((x, y)): self.saved = False
             else:
+                # ブロックを加える
                 kind = self.calc_kind(key) # 0～47のうち該当するやつを返す
                 # なければ-1が返るのでブロックは作れない。
                 if kind >= 0:
-                    self.add_block((x, y), kind)
+                    if self.add_block((x, y), kind): self.saved = False
     
     def map_init(self):
         """マップデータの初期化"""
         for i in range(13):
             for j in range(22):
                 self.stagemap[i][j] = '.'
+        self.blocks.empty()
+
+    def calc_clickevent(self, x, y):
+        if 350 < x < 430 and 270 < y < 310: return 0
+        else:
+            if (not 10 < x < 310) or (not 270 < y < 310): return -1
+            k = 15 * ((y - 270) // GS) + ((x + 10) // GS)
+            if k > 25: return -1
+        return k
 
     def calc_kind(self, key):
         """keyの値に応じてブロックのkindを返す"""
@@ -188,19 +270,17 @@ class editor():
     def add_block(self, pos, kind):
         """ブロックの追加"""
         occupied = self.calc_occupy(pos, kind)
-        if occupied == None: return
+        if occupied == None: return False  # はみ出したらNG.
         for o_pos in occupied:
             x, y = o_pos
-            if self.occupy[y][x] == 1: return  # 占有されていたらNG.
+            if self.occupy[y][x] == 1: return False # 占有されていたらNG.
         # ブロックを作る。
         block(pos, kind)
         for o_pos in occupied:
             x, y = o_pos
             self.occupy[y][x] = 1
-        if kind < 26:
-            self.stagemap[y][x] = chr(kind + 65)
-        else:
-            self.stagemap[y][x] = chr(kind - 26 + 97)
+            self.stagemap[y][x] = toletter(kind)  # kindから文字を生成
+        return True # 変化があったら報告
 
     def delete_block(self, pos):
         """ブロックの削除"""
@@ -212,7 +292,8 @@ class editor():
                     x, y = o_pos
                     self.occupy[y][x] = 0
                 block.kill()
-                break
+                return True  # 変化があったら報告
+        return False
 
     def load_image(self, filename):
         """画像のロード"""
@@ -221,9 +302,33 @@ class editor():
         image = pygame.image.load(filename).convert()
         return image
 
-    def save(self): pass
-        """ファイルに保存する"""
-        # 頭にフォルダ名/ってつけるとcdのフォルダ内に入るのでそうして。
+    def load_stage(self, stage):
+        """stage番号のデータをstagemapに読み込む"""
+        filename = "bbstage" + str(stage) + ".map"
+        filename = os.path.join("stages", filename)
+        data = []
+        fp = open(filename, "r")
+        for line in fp:
+            line = line.rstrip()
+            data.append(list(line))
+        fp.close()
+        self.map_init()  # 先に、初期化。
+        for i in range(13):
+            for j in range(22):
+                if data[i][j] == '.': continue
+                else: self.stagemap[i][j] = data[i][j]
+                block((j, i), tonum(data[i][j]))
+
+    def save_stage(self):
+        # current_stageのところにデータを保存する
+        filename = "bbstage" + str(self.current_stage) + ".map"
+        filename = os.path.join("stages", filename)
+        fp = open(filename, "w")
+        for i in range(13):
+            for j in range(22):
+                fp.write(self.stagemap[i][j])
+            fp.write("\n")
+        fp.close()
 
 class block(pygame.sprite.Sprite):
     """ブロックの定義"""
